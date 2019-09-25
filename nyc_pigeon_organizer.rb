019aafbfe84ee375b5aa51a4317ccff8329068a1
Dominic Chu <dominic.m.chu@gmail.com>
2019-09-25 06:09:49 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,9 +1,9 @@
 def nyc_pigeon_organizer(data)
-  new_hash = Hash.new 
+  new_hash = Hash.new
   data.each do |category, value|
     value.each do |choices, names_array|
       names_array.each do |name|
-        if new_hash[name] == nil 
+        if new_hash[name] == nil
           new_hash[name] = {}
         end
         if new_hash[name][category] == nil
